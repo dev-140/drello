e522d7955ab36a458ec28b2c54d6e20ed3474ba6
rox <roxmarzan14@gmail.com>
2024-06-05 18:40:56 +0800

    template completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,47 @@
 import React from "react";
 import Testimonials from "./lower-section-cards/Testimonials";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function About() {
+    var settings = {
+        dots: true,
+        speed: 1000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 4000,
+        cssEase: "cubic-bezier(0.600, -0.280, 0.735, 0.045)",
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    initialSlide: 2,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
+
     return (
         <div className='about-container container pb-5 pt-5'>
             <div className='row'>
@@ -10,7 +50,54 @@ function About() {
                         What People Say About Dwello
                     </h3>
 
-                    <Testimonials></Testimonials>
+                    <Slider {...settings}>
+                        <Testimonials
+                            imgRoom='0'
+                            imgProfile='0'
+                            name='Sarah Nguyen'
+                            location='San Francisco'
+                            rating='5.0'
+                            comment='Dwello truly cares about their clients. They listened to
+                        my needs and preferences and helped me find the perfect
+                        home in the Bay Area. Their professionalism and
+                        attention to detail are unmatched.'
+                        ></Testimonials>
+                        <Testimonials
+                            imgRoom='1'
+                            imgProfile='1'
+                            name='John Doe'
+                            location='Los Angeles'
+                            rating='4.8'
+                            comment='The team at Dwello was fantastic. They guided me through the entire process and made buying a home so much easier than I expected.'
+                        ></Testimonials>
+
+                        <Testimonials
+                            imgRoom='2'
+                            imgProfile='2'
+                            name='Emily Davis'
+                            location='New York'
+                            rating='4.9'
+                            comment='Dwello"s dedication to finding the right home for me was incredible. Their expertise and market knowledge are second to none.'
+                        ></Testimonials>
+
+                        <Testimonials
+                            imgRoom='3'
+                            imgProfile='3'
+                            name='Michael Brown'
+                            location='Chicago'
+                            rating='4.7'
+                            comment='I was impressed by Dwello"s professionalism and efficiency. They found me a great home in a very short amount of time.'
+                        ></Testimonials>
+
+                        <Testimonials
+                            imgRoom='4'
+                            imgProfile='4'
+                            name='James White'
+                            location='Miami'
+                            rating='5.0'
+                            comment='Dwello made my home-buying experience a breeze. They are knowledgeable, friendly, and truly care about their clients.'
+                        ></Testimonials>
+                    </Slider>
                 </div>
             </div>
         </div>
--- src/components/lower-section-cards/Testimonials.js
+++ src/components/lower-section-cards/Testimonials.js
@@ -1,11 +1,50 @@
 import React from "react";
 
-function Testimonials() {
+function Testimonials(props) {
+    const imagesRoom = [
+        require("../../assets/testimonial-room-1.svg").default,
+        require("../../assets/testimonial-room-2.svg").default,
+        require("../../assets/testimonial-room-3.svg").default,
+        require("../../assets/testimonial-room-4.jpeg"),
+        require("../../assets/testimonial-room-5.jpeg"),
+    ];
+
+    const imagesProfile = [
+        require("../../assets/profile-pic-1.svg").default,
+        require("../../assets/profile-pic-2.svg").default,
+        require("../../assets/profile-pic-3.svg").default,
+        require("../../assets/profile-pic-4.webp"),
+        require("../../assets/profile-pic-5.webp"),
+    ];
     return (
-        <div className='TS-card'>
-            <div className='TS-card__image'></div>
-            <div className='TS-card__info'></div>
-            <div className='TS-card__desc'></div>
+        <div className='TS-card pb-2'>
+            <div className='TS-card__image'>
+                <img src={imagesRoom[props.imgRoom]} />
+            </div>
+            <div className='TS-card__body p-3'>
+                <div className='TS-card__info mt-2 d-flex align-items-center mb-3'>
+                    <img
+                        src={imagesProfile[props.imgProfile]}
+                        className='card-profile-img'
+                    />
+                    <div className='TS-card__name d-flex flex-column ms-2'>
+                        <p className='text text--normal text--bold mb-0'>
+                            {props.name}
+                        </p>
+                        <p className='text text--main mb-0'>{props.location}</p>
+                    </div>
+
+                    <div className='TS-card__rating d-flex aling-items-center p-1 ms-auto'>
+                        <i className='bi bi-star-fill star'></i>
+                        <p className='text text--16 mb-0 rating ms-2'>
+                            {props.rating}
+                        </p>
+                    </div>
+                </div>
+                <div className='TS-card__desc'>
+                    <p className='text text--16 mb-0'>{props.comment}</p>
+                </div>
+            </div>
         </div>
     );
 }
